ea12d4c8576df85caaed0078a70866cf03ac065d
kurigohan <shehamane@mail.ru>
2020-09-30 15:37:46 +0300

    test fixed

--- src/tests/square.cpp
+++ src/tests/square.cpp
@@ -2,15 +2,32 @@
 #include "../lib/Square.h"
 
 #include <iostream>
+#include <atomic>
+#include <csignal>
+#include <cstring>
 
+std::atomic<bool> quit(false);
+
+void got_signal(int) {
+    quit.store(true);
+}
 
 int main(){
-    std::setlocale(LC_ALL, "en_US.UTF-8");    try{
+    {
+        struct sigaction sa{};
+        memset(&sa, 0, sizeof(sa));
+        sa.sa_handler = got_signal;
+        sigfillset(&sa.sa_mask);
+        sigaction(SIGINT, &sa, nullptr);
+    }
+
+    try{
         Screen screen(200, 200);
         screen.setFPS(60);
-        Square s(screen, 5);
+        Square s(&screen, 5);
         while(true){
             s.keyHandler();
+            if (quit.load()) break;
         }
     } catch (std::exception e) {
         std::cout << e.what();
